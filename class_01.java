ea8466b8fb40d67e9d6c63221656028da250f105
wbhongggg <92263847+wbhongggg@users.noreply.github.com>
2021-11-01 12:06:12 +0800

    Add files via upload

---
+++ class_01.java
@@ -0,0 +1,52 @@
+package teat;
+class Cal_area
+{
+	int length;
+	int width;
+	int height;
+	int radius;
+	private double pi=3.14159;
+	void setL(int l)
+	{
+		length=l;
+	}
+	void setW(int w)
+	{
+		width=w;
+	}
+	void setH(int h)
+	{
+		height=h;
+	}
+	void setR(int r)
+	{
+		radius=r;
+	}
+	void setCube(int l,int w,int h)
+	{
+		length=l;
+		width=w;
+		height=h;
+	}
+	void show()
+	{
+		System.out.println("半徑="+radius);
+		System.out.println("球型體積="+pi*radius*radius*radius*4/3);
+		System.out.println("長="+length+",寬="+width+",高="+height);
+		System.out.println("立方體表面積="+length*width*6);
+	}
+}
+public class class_01
+{
+
+	public static void main(String[] args)
+	{
+		Cal_area ca=new Cal_area();
+		ca.setL(4);
+		ca.setW(5);
+		ca.setH(6);
+		ca.setR(10);
+		ca.show();
+	}
+
+}
